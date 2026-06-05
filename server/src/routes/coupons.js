3cec851db2aea7f265e355ff8d5b0846dd5f06f4
A7mad110 <a7mad@example.com>
2026-06-05 14:09:31 +0200

    Remove auth middleware from coupon validate route

--- server/src/routes/coupons.js
+++ server/src/routes/coupons.js
@@ -3,6 +3,6 @@ const router = Router();
 const couponCtrl = require('../controllers/couponController');
 const { auth } = require('../middleware/auth');
 
-router.post('/validate', auth, couponCtrl.validateCoupon);
+router.post('/validate', couponCtrl.validateCoupon);
 
 module.exports = router;
